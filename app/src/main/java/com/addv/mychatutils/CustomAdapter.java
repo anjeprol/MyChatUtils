d6d97137d97d9738c7a250b5c66aaec3ef30ec3b
antonio prado <antonio.prado@amk-technologies.com>
2020-09-04 15:38:57 -0500

    version base

---
+++ app/src/main/java/com/addv/mychatutils/CustomAdapter.java
@@ -0,0 +1,48 @@
+package com.addv.mychatutils;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private ArrayList<String> names;
+
+    public CustomAdapter(Context context, int layout, ArrayList<String> names){
+        this.context = context;
+        this.layout = layout;
+        this.names = names;
+    }
+
+    @Override
+    public int getCount() {
+        return this.names.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.names.get(position);
+    }
+
+    @Override
+    public long getItemId(int id) {
+        return id;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        View v = convertView;
+        LayoutInflater layoutInflater = LayoutInflater.from(this.context);
+        v = layoutInflater.inflate(R.layout.list_item, null);
+        String currentName = names.get(position);
+        TextView textView = (TextView) v.findViewById(R.id.textview);
+        textView.setText(currentName);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/addv/mychatutils/MainActivity.java
@@ -0,0 +1,84 @@
+package com.addv.mychatutils;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listview;
+    private ArrayList<String> names;
+    private int profile [];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        listview = (ListView) findViewById(R.id.simpleListView);
+
+        names = new ArrayList<String>();
+        names.add("Veracruz");
+        names.add("Tabasco");
+        names.add("Chiapas");
+        names.add("Campeche");
+
+        CustomAdapter customAdapter = new CustomAdapter(this, R.layout.list_item, names);
+        listview.setAdapter(customAdapter);
+
+        setSupportActionBar(toolbar);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(MainActivity.this, "Has pulsado: "+ names.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
